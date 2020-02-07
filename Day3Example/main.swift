4cadf202a1c3d424a244ba0fcfcc2b251f0c122e
kritima <kritimakukreja5@gmail.com>
2020-02-07 17:54:57 -0500

    Initial Commit

---
+++ Day3Example/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day3Example
+//
+//  Created by Kritima Kukreja on 2020-02-07.
+//  Copyright © 2020 Kritima Kukreja. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
